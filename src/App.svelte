66a493ef1d13ad6399da1484bc86fb0db3511dd3
toy <escusado@gmail.com>
2020-05-17 15:17:17 -0500

    dynamic dependency loading

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,37 @@
 <script>
   import Text from "components/Text.svelte";
-  import Scene from "components/Scene.svelte";
+  import MainScene from "scenes/Main.svelte";
 
-  export let name;
+  const Dependencies = [
+    "//aframe.io/releases/1.0.4/aframe.js",
+    "//cdn.rawgit.com/donmccurdy/aframe-physics-system/v4.0.1/dist/aframe-physics-system.min.js",
+    "//unpkg.com/aframe-environment-component@1.1.0/dist/aframe-environment-component.min.js"
+  ];
 
-  let isAframeLoaded = false;
-
-  const aFrameLoaded = () => {
-    isAframeLoaded = true;
-  };
-
-  let loaded = 0;
-  const onLoad = () => {
-    loaded++;
+  // ordered dependency loader
+  let dependenciesLoaded = false;
+  let reloading = true;
+  let currentDependency = 0;
+  const onDependencyLoad = () => {
+    reloading = false;
+    currentDependency++;
+    if (currentDependency === Dependencies.length) {
+      dependenciesLoaded = true;
+    }
+    setTimeout(() => (reloading = true), 0); // force re-render to load new js
   };
 </script>
 
 <svelte:head>
-  <script
-    src="https://aframe.io/releases/1.0.4/aframe.js"
-    on:load={aFrameLoaded}>
-
-  </script>
-  {#if isAframeLoaded}
-    <script
-      src="//cdn.rawgit.com/donmccurdy/aframe-physics-system/v4.0.1/dist/aframe-physics-system.min.js"
-      on:load={onLoad}>
-
-    </script>
-    <script
-      src="https://unpkg.com/aframe-environment-component@1.1.0/dist/aframe-environment-component.min.js"
-      on:load={onLoad}>
+  {#if reloading}
+    <script src={Dependencies[currentDependency]} on:load={onDependencyLoad}>
 
     </script>
   {/if}
-
 </svelte:head>
 
 <main>
-  {#if loaded === 2}
-    <Scene />
+  {#if dependenciesLoaded}
+    <MainScene />
   {/if}
 </main>
